2910c4ff0df73a0b4ecf3dce1db60dc824ed7d84
ValentineLgd <valentinelagand@gmail.com>
2021-07-12 16:45:50 +0000

    With the tutoring help, fixed the javascript code inside bag.html to make the 'update' quantity button works

--- bag/views.py
+++ bag/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
@@ -25,7 +24,6 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 

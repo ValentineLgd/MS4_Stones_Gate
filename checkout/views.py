48247955332cca6ec6b7221f088beed7f4e72338
ValentineLgd <valentinelagand@gmail.com>
2021-07-14 08:19:56 +0000

    added stripe to payment form & designed it

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ip8wcErc69KbLOdN3dTCepgcdseVY2Kc70gV0zQNBAO0tX4087WG59MUO7fq1wPnyp88ccdr0G3McrRAaQnfCCJ00X5Dir3WP',
+        'client_secret': 'test client secret,'
     }
 
     return render(request, template, context)

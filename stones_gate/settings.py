b90ab9a45d2edbb344bd67330c5be18a4433058f
ValentineLgd <valentinelagand@gmail.com>
2021-07-21 09:45:33 +0000

    deployment part 2

--- stones_gate/settings.py
+++ stones_gate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a0q40#9+thw4-!2bg6%-$y*bjw%=1@t*@k)-j_e2*ht1hh(nlo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-stones-gate.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'stones_gate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

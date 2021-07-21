b2d57601ade7ac2ece837ca2d4869dbf14fce94e
ValentineLgd <valentinelagand@gmail.com>
2021-07-21 09:55:12 +0000

    tentative

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
-
 from checkout.models import Order
 
 @login_required

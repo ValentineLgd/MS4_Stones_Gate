76625160356940cf9021487ddf806b1855779ba7
ValentineLgd <valentinelagand@gmail.com>
2021-07-21 09:32:22 +0000

    deployment part 1

--- stones_gate/settings.py
+++ stones_gate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -126,7 +127,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
 
 
 # Password validation
